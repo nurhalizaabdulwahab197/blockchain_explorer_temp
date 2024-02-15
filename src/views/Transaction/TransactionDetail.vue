c67cb11887119bb4618d176e298595f3b67569e6
Loy <tanzenchai0923@gmail.com>
2024-02-15 16:00:46 +0800

    feat: Update frontend

--- src/views/Transaction/TransactionDetail.vue
+++ src/views/Transaction/TransactionDetail.vue
@@ -10,16 +10,16 @@
       <div class="firstrow-container">
         <div class="column-container">
           <div class="container-title">
-            <div>TRANSACTION ID</div>
+            <div class="firstrowtitle">TRANSACTION ID</div>
             <button class="rowCopybutton">
-              <div></div>
-              <div>CLICK TO COPY</div>
+              <span class="iconamoon--copy-bold"></span>
+              <div class="none">CLICK TO COPY</div>
             </button>
           </div>
           <div>{{ transactionId }}</div>
         </div>
         <div class="column-container">
-          <div class="container-title">TIMESTAMP</div>
+          <div class="container-title firstrowtitle">TIMESTAMP</div>
           <div class="center">{{ timestamp }}</div>
         </div>
       </div>
@@ -29,101 +29,117 @@
           <div class="block">1837445</div>
         </div>
       </div>
-      <table>
-        <tr class="tabletitle-container">
-          <th colspan="2">
-            <div class="tabletitle">
-              <span class="dashicons--money-alt"></span>
-              <div>TRANSACTION DETAILS</div>
-            </div>
-          </th>
-        </tr>
-        <tr>
-          <td>
-            <div class="tablesubtitle">
-              <span class="dashicons--money-alt2"></span>
-              <div>SENDER : </div>
-            </div>
-          </td>
-          <td class="tablerow">
-            <a href="">0x3Css</a>
-            <button class="tableCopybutton">
-              <div></div>
-              <div>CLICK TO COPY</div>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div class="tablesubtitle">
-              <span class="dashicons--money-alt2"></span>
-              <div>AMOUNT :</div>
-            </div>
-          </td>
-          <td colspan="2" class="amount">0.035 ETH</td>
-        </tr>
-        <tr>
-          <td>
-            <div class="tablesubtitle">
-              <span class="dashicons--money-alt2"></span>
-              <div>RECERIVER :</div>
-            </div>
-          </td>
-          <td class="tablerow">
-            <a href="">0x3Css</a>
-            <button class="tableCopybutton">
-              <div></div>
-              <div>CLICK TO COPY</div>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div class="tablesubtitle">
-              <span class="dashicons--money-alt2"></span>
-              <div>VALUE :</div>
-            </div>
-          </td>
-          <td>
-            <div class="alligncenter">
-              <span class="ri--eth-fill"></span>
-              <div>0 ETH($0.00) </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div class="tablesubtitle">
-              <span class="dashicons--money-alt2"></span>
-              <div>GAS PRICE :</div>
-            </div>
-          </td>
-          <td>0.023123123 ETH ($0.63)</td>
-        </tr>
-        <tr>
-          <td class="feetitle">
-            <div class="tablesubtitle">
-              <span class="dashicons--money-alt2"></span>
-              <div>TRANSACTION FEE :</div>
-            </div>
-          </td>
-          <td>
-            <div class="transactionfee-content">
-              <div>6.023123123 Gwei (0.000012321 ETH)</div>
-              <div class="transactionfee-container">
-                <div class="column2-container">
-                  <div class="container-title">NOTE</div>
-                  <div class="center">This sender has created a contract</div>
+      <div style="overflow-x: auto">
+        <table>
+          <tr class="tabletitle-container">
+            <th colspan="2">
+              <div class="tabletitle">
+                <span class="dashicons--money-alt"></span>
+                <div>TRANSACTION DETAILS</div>
+              </div>
+            </th>
+          </tr>
+          <tr>
+            <td>
+              <div class="tablesubtitle">
+                <div class="center">
+                  <span class="dashicons--money-alt2"></span>
                 </div>
-                <div class="column2-container">
-                  <div class="container-title">ONCOMPLETE</div>
-                  <div class="center">Create</div>
+                <div>SENDER : </div>
+              </div>
+            </td>
+            <td class="tablerow">
+              <a href="" class="tablecontent">0x3Css</a>
+              <button class="tableCopybutton">
+                <span class="iconamoon--copy-bold"></span>
+                <div class="none">CLICK TO COPY</div>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="tablesubtitle">
+                <div class="center">
+                  <span class="dashicons--money-alt2"></span>
+                </div>
+                <div>AMOUNT :</div>
+              </div>
+            </td>
+            <td colspan="2" class="amount tablecontent">0.035 ETH</td>
+          </tr>
+          <tr>
+            <td>
+              <div class="tablesubtitle">
+                <div class="center">
+                  <span class="dashicons--money-alt2"></span>
+                </div>
+                <div>RECERIVER :</div>
+              </div>
+            </td>
+            <td class="tablerow">
+              <a href="" class="tablecontent">0x3Css</a>
+              <button class="tableCopybutton">
+                <span class="iconamoon--copy-bold"></span>
+                <div class="none">CLICK TO COPY</div>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="tablesubtitle">
+                <div class="center">
+                  <span class="dashicons--money-alt2"></span>
+                </div>
+                <div>VALUE :</div>
+              </div>
+            </td>
+            <td>
+              <div class="alligncenter">
+                <span class="ri--eth-fill"></span>
+                <div class="tablecontent">0 ETH($0.00) </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="tablesubtitle">
+                <div class="center">
+                  <span class="dashicons--money-alt2"></span>
+                </div>
+                <div>GAS PRICE :</div>
+              </div>
+            </td>
+            <td>
+              <div class="tablecontent"> 0.023123123 ETH ($0.63) </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="feetitle">
+              <div class="tablesubtitle">
+                <div class="center">
+                  <span class="dashicons--money-alt2"></span>
+                </div>
+                <div>TRANSACTION FEE :</div>
+              </div>
+            </td>
+            <td>
+              <div class="transactionfee-content">
+                <div class="tablecontent">6.023123123 Gwei (0.000012321 ETH)</div>
+                <div class="transactionfee-container">
+                  <div class="column2-container">
+                    <div class="container-title">NOTE</div>
+                    <div class="center">This sender has created a contract</div>
+                  </div>
+                  <div class="column2-container">
+                    <div class="container-title">ONCOMPLETE</div>
+                    <div class="center">Create</div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </td>
-        </tr>
-      </table>
+            </td>
+          </tr>
+        </table>
+      </div>
       <div>
         <div class="subTitle">
           <span class="dashicons--money-alt"></span>
@@ -132,14 +148,18 @@
         </div>
         <div class="moredetail-container">
           <div class="left-container">
-            <span class="dashicons--money-alt2"></span>
+            <div class="center">
+              <span class="dashicons--money-alt2"></span>
+            </div>
             <div>Gas Limit & Usage by Txn:</div>
           </div>
           <div class="right-container">65,00 | 43221 (98%)</div>
         </div>
         <div class="moredetail-container">
           <div class="left-container">
-            <span class="dashicons--money-alt2"></span>
+            <div class="center">
+              <span class="dashicons--money-alt2"></span>
+            </div>
             <div>Gas Fees:</div>
           </div>
           <div class="right-container">65,00 | 43221 (98%)</div>
@@ -216,6 +236,7 @@ watch(
   padding-left: 1.5rem;
   margin: 0;
   gap: 0.2rem;
+  justify-content: left;
   align-items: center;
 }
 
@@ -239,6 +260,21 @@ watch(
   border-radius: 50%;
   justify-content: center;
   align-items: center;
+}
+
+.iconamoon--copy-bold {
+  --svg: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='2.5'%3E%3Cpath d='M16 3H4v13'/%3E%3Cpath d='M8 7h12v12a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2z'/%3E%3C/g%3E%3C/svg%3E");
+
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  background-color: currentcolor;
+  mask-image: var(--svg);
+  mask-image: var(--svg);
+  mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+  mask-size: 100% 100%;
+  mask-size: 100% 100%;
 }
 
 .material-symbols--navigate-back {
@@ -306,6 +342,7 @@ a {
   border-radius: 0.5rem;
   justify-content: center;
   align-items: center;
+  gap: 0.5rem;
 }
 
 .rowCopybutton:hover {
@@ -324,6 +361,7 @@ a {
   border-radius: 0.5rem;
   justify-content: center;
   align-items: center;
+  gap: 0.5rem;
 }
 
 .tableCopybutton:hover {
@@ -382,6 +420,7 @@ td {
 
 .center {
   display: flex;
+  font-size: 0.8rem;
   justify-content: center;
 }
 
@@ -393,13 +432,14 @@ td {
 
 .firstrow-container {
   display: flex;
+  overflow-x: auto;
   justify-content: space-between;
   gap: 2rem;
 }
 
 .column2-container {
   display: grid;
-  width: 100%;
+  width: 9rem;
   padding: 0.5rem 1rem 0.7rem;
   color: black;
   background-color: rgb(200 200 200);
@@ -482,5 +522,110 @@ td {
 
 .amount {
   color: rgb(24 255 24);
+}
+
+@media screen and (width <= 828px) {
+  .title {
+    margin-right: 0.5rem;
+    margin-left: 0.5rem;
+    font-size: 1.1rem;
+  }
+
+  .rowCopybutton {
+    padding: 0.2rem 0.8rem;
+    margin-left: 1rem;
+  }
+
+  .tableCopybutton {
+    padding: 0.2rem 0.8rem;
+  }
+
+  .none {
+    display: none;
+  }
+
+  .dashicons--money-alt {
+    --svg: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3E%3Cpath fill='%23000' d='M10.6 9c-.4-.1-.8-.3-1.1-.6c-.3-.1-.4-.4-.4-.6c0-.2.1-.5.3-.6c.3-.2.6-.4.9-.3c.6 0 1.1.3 1.4.7l.9-1.2c-.3-.3-.6-.5-.9-.7c-.3-.2-.7-.3-1.1-.3V4H9.4v1.4c-.5.1-1 .4-1.4.8c-.4.5-.7 1.1-.6 1.7c0 .6.2 1.2.6 1.6c.5.5 1.2.8 1.8 1.1c.3.1.7.3 1 .5c.2.2.3.5.3.8c0 .3-.1.6-.3.9c-.3.3-.7.4-1 .4c-.4 0-.9-.1-1.2-.4c-.3-.2-.6-.5-.8-.8l-1 1.1c.3.4.6.7 1 1c.5.3 1.1.6 1.7.6V16h1.1v-1.5c.6-.1 1.1-.4 1.5-.8c.5-.5.8-1.3.8-2c0-.6-.2-1.3-.7-1.7c-.5-.5-1-.8-1.6-1M10 2c-4.4 0-8 3.6-8 8s3.6 8 8 8s8-3.6 8-8s-3.6-8-8-8m0 14.9c-3.8 0-6.9-3.1-6.9-6.9S6.2 3.1 10 3.1s6.9 3.1 6.9 6.9s-3.1 6.9-6.9 6.9'/%3E%3C/svg%3E");
+
+    display: inline-block;
+    width: 1.2rem;
+    height: 1.2rem;
+    background-color: currentcolor;
+    mask-image: var(--svg);
+    mask-repeat: no-repeat;
+    mask-size: 100% 100%;
+  }
+
+  .iconnextbutton {
+    display: flex;
+    width: 1.2rem;
+    height: 1.2rem;
+    margin-left: 0.3rem;
+    background-color: rgb(217 217 217 / 30%);
+    border-radius: 50%;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .iconbackbutton {
+    display: flex;
+    width: 1.2rem;
+    height: 1.2rem;
+    margin-left: 0.3rem;
+    background-color: rgb(217 217 217 / 30%);
+    border-radius: 50%;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .tablesubtitle {
+    display: flex;
+    padding-left: 1.2rem;
+    margin: 0;
+    font-size: 0.8rem;
+    gap: 0.2rem;
+    align-items: center;
+    justify-content: left;
+  }
+
+  .column-container {
+    display: grid;
+    width: 100%;
+    padding: 0.8rem 1rem;
+    color: black;
+    background-color: rgb(200 200 200);
+    border-radius: 0.3rem;
+    row-gap: 1rem;
+  }
+
+  .left-container {
+    display: flex;
+    align-items: center;
+    padding-left: 0.3rem;
+    gap: 0.5rem;
+    flex: 0 0 45%;
+  }
+
+  .tabletitle {
+    display: flex;
+    gap: 0.7rem;
+    font-size: 1.2rem;
+    align-items: center;
+  }
+
+  .firstrow-container {
+    display: flex;
+    overflow-x: auto;
+    justify-content: space-between;
+    gap: 1rem;
+  }
+
+  .tablecontent {
+    font-size: 1.1rem;
+  }
+
+  td:first-child {
+    width: 11rem;
+  }
 }
 </style>
